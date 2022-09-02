c4f372d683cae3c3ff915bcdedc07ab5e7c9af71
Youssef Touate <ytouate@e1r6p1.1337.ma>
2022-09-02 12:37:46 +0100

    fixing the timestamp horrible error i made

--- module_0/ex02/Account.cpp
+++ module_0/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: ytouate < ytouate@student.1337.ma>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 09:25:24 by ytouate           #+#    #+#             */
-/*   Updated: 2022/09/01 12:13:55 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/09/02 12:37:14 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,17 @@ Account::Account(int initial_deposit) {
 }
 
 void    Account::_displayTimestamp(void) {
-    struct timeval time;
-
-    gettimeofday(&time, NULL);
-    std::cout << "[" << time.tv_sec << "_" << time.tv_usec << "] ";
+    time_t time;
+    time = std::time(NULL);
+    tm *local_time = std::localtime(&time);
+    std::cout << "["
+                << local_time->tm_year + 1900
+                << 1 + local_time->tm_mon 
+                << local_time->tm_mday
+                << "_" 
+                << local_time->tm_hour
+                << local_time->tm_min 
+                << local_time->tm_sec << "] ";
 }
 
 void    Account::displayAccountsInfos(void) {

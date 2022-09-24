74482e7de6e52abbb92574b81e799a01f9aa2f14
ytouate <touateyoussef2003@gmail.com>
2022-09-24 20:42:13 +0100

    implementing the wrongCat and wrongAnimal classes

--- module_4/ex00/WrongAnimal.hpp
+++ module_4/ex00/WrongAnimal.hpp
@@ -1,12 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*   WrongAnimal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/24 20:38:22 by ytouate           #+#    #+#             */
-/*   Updated: 2022/09/24 20:38:23 by ytouate          ###   ########.fr       */
+/*   Created: 2022/09/21 18:47:36 by ytouate           #+#    #+#             */
+/*   Updated: 2022/09/24 20:38:43 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WrongAnimal_H
+#define WrongAnimal_H
+
+#include <iostream>
+
+class WrongAnimal
+{
+protected:
+    std::string type;
+
+public:
+    WrongAnimal();
+    WrongAnimal(std::string type);
+    WrongAnimal(WrongAnimal &);
+    WrongAnimal &operator=(WrongAnimal &);
+    std::string getType(void) const;
+    virtual void makeSound() const;
+    virtual ~WrongAnimal();
+};
+
+#endif
---
+++ module_4/ex00/WrongCat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 18:52:11 by ytouate           #+#    #+#             */
+/*   Updated: 2022/09/23 17:00:51 by ytouate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WrongCat_HPP
+#define WrongCat_HPP
+
+#include "Animal.hpp"
+
+class WrongCat : public Animal
+{
+public:
+    WrongCat();
+    WrongCat(std::string type);
+    WrongCat(WrongCat &);
+    WrongCat &operator=(WrongCat &);
+    void makeSound() const;
+    ~WrongCat();
+};
+#endif

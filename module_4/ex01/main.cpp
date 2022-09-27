69704224af7b4958a8123f47dd9e5ad65aba4a8d
ytouate <touateyoussef2003@gmail.com>
2022-09-27 23:26:40 +0100

    decomenting some tests

--- module_4/ex01/main.cpp
+++ module_4/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 18:47:26 by ytouate           #+#    #+#             */
-/*   Updated: 2022/09/27 23:23:33 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/09/27 23:26:25 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,19 +47,18 @@ int main()
         std::cout << "\nanimalsArray[x] Idea: |" << animalsArray[x]->getBrain()->getIdea(0) << "|" << std::endl;
         // deleting the cat and dog object as if they were Animal objects
     }
-    // // creating an object from animalArray[0] which is a Cat in this case
-    // std::cout << "\n====Copy Constructors====" << std::endl;
-    // Animal *random = new Animal(*animalsArray[0]);
-    // for (int x = 0; x < ANIMALS_COUNT; x++)
-    //     delete animalsArray[x];
-    // std::cout << "\n====Deep Copy====" << std::endl;
-    // // deleting the array which contains the source of the "random" object;
-    // // delete [] animalsArray;
-    // std::cout << "Random Type: " << random->getType() << std::endl; // accessing the type
-    // // accessing the Brain object which should not throw a problem
-    // std::cout << "Random First Idea: " << random->getBrain()->getIdea(0) << std::endl;
-    // random->makeSound();
-    // std::cout << "\n====Destruction===" << std::endl;
-    // delete random;
+    // creating an object from animalArray[0] which is a Cat in this case
+    std::cout << "\n====Copy Constructors====" << std::endl;
+    Animal *random = new Animal(*animalsArray[0]);
+    for (int x = 0; x < ANIMALS_COUNT; x++)
+        delete animalsArray[x];
+    std::cout << "\n====Deep Copy====" << std::endl;
+    // deleting the array which contains the source of the "random" object;
+    std::cout << "Random Type: " << random->getType() << std::endl; // accessing the type
+    // accessing the Brain object which should not throw a problem
+    std::cout << "Random First Idea: " << random->getBrain()->getIdea(0) << std::endl;
+    random->makeSound();
+    std::cout << "\n====Destruction===" << std::endl;
+    delete random;
     return 0;
 }

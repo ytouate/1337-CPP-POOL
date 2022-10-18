9de8d86f69e781da4bba24374f01f034c21dfba7
ytouate <touateyoussef2003@gmail.com>
2022-10-18 16:27:12 +0100

    ex00 is maybe done

--- module_6/ex01/utils.cpp
+++ module_6/ex01/utils.cpp
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 18:32:18 by ytouate           #+#    #+#             */
-/*   Updated: 2022/10/18 16:26:27 by ytouate          ###   ########.fr       */
+/*   Created: 2022/10/18 16:20:31 by ytouate           #+#    #+#             */
+/*   Updated: 2022/10/18 16:24:23 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Data.hpp"
 
-int main()
+uintptr_t serialize(Data *ptr)
 {
-    Data *data;
+    return reinterpret_cast<uintptr_t>(ptr);
+}
 
-    data = new Data();
-
-    uintptr_t val;
-
-    std::cout << data->getAge() << " " << data->getName() << std::endl;
-    val = serialize(data);
-    Data *something = deserialize(val);
-    std::cout << something->getAge() << " " << something->getName() << std::endl;
-    return 0;
+Data *deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data *>(raw);
 }

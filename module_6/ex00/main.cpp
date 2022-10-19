6cd051c47e3e258a3f81352d85f635e7a4624767
ytouate <touateyoussef2003@gmail.com>
2022-10-19 10:19:31 +0100

    checking for empty string in ex00

--- module_6/ex00/main.cpp
+++ module_6/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 18:32:18 by ytouate           #+#    #+#             */
-/*   Updated: 2022/10/19 09:55:54 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/10/19 10:19:02 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ int main()
 {
     std::string arg;
     std::getline(std::cin, arg);
-    int prc = count_precision(arg);
-    print_char(arg);
-    print_int(arg);
-    print_float(arg, prc);
-    print_double(arg);
+    if (!arg.empty())
+    {
+        int prc = count_precision(arg);
+        print_char(arg);
+        print_int(arg);
+        print_float(arg, prc);
+        print_double(arg);
+    }
+    else
+        std::cout << "INSERT A NON-EMPTY STRING\n";
 }
+    

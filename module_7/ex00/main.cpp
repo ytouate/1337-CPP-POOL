b14cfeacb9a90ac44db10f752f62c77a1890f884
ytouate <touateyoussef2003@gmail.com>
2022-10-20 15:01:43 +0100

    ex00 is done if we take the provided test as refrence

---
+++ module_7/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:46:53 by ytouate           #+#    #+#             */
+/*   Updated: 2022/10/20 14:59:34 by ytouate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main(void)
+{
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}
---
+++ module_7/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:47:14 by ytouate           #+#    #+#             */
+/*   Updated: 2022/10/20 15:00:55 by ytouate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#if !defined(MACRO)
+#define MACRO
+
+template <typename T>
+void swap(T &x, T &y)
+{
+    T z;
+    
+    z = x;
+    x = y;
+    y = z;
+}
+
+template <typename T>
+T min(T x, T y)
+{
+    return ((x == y) ? y : (x < y) ? x : y);
+}
+
+template <typename T>
+T max(T x, T y)
+{
+    return ((x == y) ? y : (x > y) ? x : y);
+}
+
+#endif // MACRO

43815ec3c4634ac312fd494d701ffd55218cb595
ytouate <touateyoussef2003@gmail.com>
2022-10-21 19:00:25 +0100

    adding more tests to the ex01

--- module_7/ex01/main.cpp
+++ module_7/ex01/main.cpp
@@ -6,17 +6,34 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:44:09 by ytouate           #+#    #+#             */
-/*   Updated: 2022/10/20 17:02:57 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/10/21 18:59:52 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void applyString (std::string &s)
+{
+    s.append(" Something");
+}
+
+void applyInt (int &n)
+{
+    n *= 2;
+}
+
 int main()
 {
-    std::string *s = new std::string[3];
-    s[0] = "Youssef";
-    s[1] = "Touate";
-    s[2] = "1337";
-    iter(s, 3, apply);
+    
+    std::string s[3] = {"hello", "world", "Name"};
+    iter(s, 3, applyString);
+    std::cout << "After applying iter\n";
+    for (int i = 0; i < 3; i++)
+        std::cout << s[i] << "\n";
+    std::cout << std::endl;
+    int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    iter(nums, 10, applyInt);
+    std::cout << "After applying iter\n";
+    for (int i = 0; i < 10; i++)
+        std::cout << nums[i] << "\n";
 }

60be9d9a682682a7318e1f5f98b05fcd2405e999
ytouate <touateyoussef2003@gmail.com>
2022-10-22 12:09:54 +0100

    ex02 is done

---
+++ module_7/ex02/Array.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 17:08:52 by ytouate           #+#    #+#             */
+/*   Updated: 2022/10/21 21:49:50 by ytouate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#if !defined(ARRAY_HPP)
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+    T *array;
+    int length;
+
+public:
+    Array()
+    {
+        std::cout << "am here\n";
+        this->array = new T[0];
+        this->length = 0;
+    }
+    Array(unsigned int n)
+    {
+        this->array = new T[n];
+        this->length = n;
+        for (int i = 0; i < this->length; i++)
+            memset(&array[i], 0, sizeof(array[i]));
+    }
+    T &operator=(const Array &rhs)
+    {
+        std::cout << "here\n";
+        for (int i = 0; i < this->length; i++)
+            this->array[i] = rhs[i];
+    }
+    int size(void) const
+    {
+        return this->length;
+    }
+    T &operator[](int idx) const
+    {
+        if (idx < 0 or idx >= this->length)
+            throw(std::out_of_range("Index out of range"));
+        return this->array[idx];
+    }
+    Array(const Array &obj)
+    {
+        this->length = obj.size();
+        this->array = new T[this->length];
+        for (int i = 0; i < this->length; i++)
+            (*this)[i] = obj[i];
+    }
+    ~Array()
+    {
+        delete [] this->array;
+    }
+};
+
+#endif // ARRAY_HP
---
+++ module_7/ex02/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 17:08:40 by ytouate           #+#    #+#             */
+/*   Updated: 2022/10/21 21:50:07 by ytouate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+#define MAX_VAL 750
+int main()
+{
+    Array<int> nums(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int val = rand();
+        nums[i] = val;
+        mirror[i] = val;
+    }
+    {
+        Array<int> tmp = nums;
+        Array<int> test(tmp);
+    }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != nums[i])
+        {
+            std::cerr << "didn't save the same value !!" << std::endl;
+            return 1;
+        }
+    }
+    try {
+        nums[-2] = 0;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+    try
+    {
+        nums[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        nums[i] = rand();
+    }
+    delete [] mirror;
+}

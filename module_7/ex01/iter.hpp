9dc731fdefa392cb5f6ee2bf127fee58eac6fdf6
ytouate <touateyoussef2003@gmail.com>
2022-10-22 14:28:45 +0100

    adding more tests and a template function overload for const refrence case

--- module_7/ex01/iter.hpp
+++ module_7/ex01/iter.hpp
@@ -14,7 +14,16 @@
 #define ITER_HPP
 #include <iostream>
 template <typename T>
-void iter(T *array, int size, void(*f)(T&))
+void iter(T *array, int size, void(*f)(T const &))
+{
+    for (int i = 0; i < size; i++)
+    {
+        (*f)(array[i]);
+    }
+}
+
+template <typename T>
+void iter(T *array, int size, void(*f)(T &))
 {
     for (int i = 0; i < size; i++)
     {
--- module_7/ex01/main.cpp
+++ module_7/ex01/main.cpp
@@ -6,34 +6,63 @@
 /*   By: ytouate <ytouate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:44:09 by ytouate           #+#    #+#             */
-/*   Updated: 2022/10/21 18:59:52 by ytouate          ###   ########.fr       */
+/*   Updated: 2022/10/21 21:41:50 by ytouate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-void applyString (std::string &s)
+class Awesome
+{
+public:
+    Awesome(void) : _n(42) { return; }
+    int get(void) const { return this->_n; }
+
+private:
+    int _n;
+};
+
+std::ostream &operator<<(std::ostream &o, Awesome const &rhs)
+{
+    o << rhs.get();
+    return o;
+}
+
+template <typename T>
+void print(T const &x)
+{
+    std::cout << x << std::endl;
+    return;
+}
+
+void applyString(std::string &s)
 {
     s.append(" Something");
 }
 
-void applyInt (int &n)
+void applyInt(int &n)
 {
     n *= 2;
 }
 
 int main()
 {
-    
+    std::cout << " ===== My tests ==== \n";
     std::string s[3] = {"hello", "world", "Name"};
     iter(s, 3, applyString);
     std::cout << "After applying iter\n";
     for (int i = 0; i < 3; i++)
         std::cout << s[i] << "\n";
     std::cout << std::endl;
-    int nums[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     iter(nums, 10, applyInt);
     std::cout << "After applying iter\n";
     for (int i = 0; i < 10; i++)
         std::cout << nums[i] << "\n";
+    std::cout << " ==== their tests ====\n" << std::endl;
+    int tab[] = {0, 1, 2, 3, 4};
+    Awesome tab2[5];
+    iter(tab, 5, print);
+    iter(tab2, 5, print);
+    return 0;
 }

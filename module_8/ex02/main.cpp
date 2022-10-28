3631d692be8c512d2f6f5abe69f7201f5545a30e
ytouate <touateyoussef2003@gmail.com>
2022-10-28 12:14:37 +0100

    all the modules are validated it was fun

--- module_8/ex02/main.cpp
+++ module_8/ex02/main.cpp
@@ -23,8 +23,8 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-
     mstack.push(0);
+
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
